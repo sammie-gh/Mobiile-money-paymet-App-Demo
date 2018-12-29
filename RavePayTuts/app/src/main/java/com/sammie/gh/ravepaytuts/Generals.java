b34cc570b1ac8547f83dd01019ba6935bfa7299f
sammie-gh <ofori.d.evans@gmail.com>
2018-12-29 02:26:44 +0100

    Update Generals.java

--- RavePayTuts/app/src/main/java/com/sammie/gh/ravepaytuts/Generals.java
+++ RavePayTuts/app/src/main/java/com/sammie/gh/ravepaytuts/Generals.java
@@ -5,8 +5,8 @@ public class Generals {
     public static final String EMAIL_KEY = "evansdrah@gmail.com";
     public static final String USERNAME_KEY= "Sammie";
 
-    public static final String PUBLIC_KEY= "FLWPUBK-eb918dc6f43f9b10624d3cdd093fe172-X";
-    public static final String ENCRY_KEY= "58274e09137a54bbc4979eaa";
+    public static final String PUBLIC_KEY= "FLWPUBK-1984170974b9e39dd5860bd306c730fa-X";
+    public static final String ENCRY_KEY= "aaea0509816633b1ab46b398";
 
 
 }
